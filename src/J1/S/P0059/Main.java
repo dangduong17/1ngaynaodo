279c0edc877bf02508526bc88cf9d42193f1b9f0
Duong <duongdmhe141178@fpt.edu.vn>
2022-05-24 12:00:01 +0700

    DUong dep zai vl

---
+++ src/J1/S/P0059/Main.java
@@ -0,0 +1,20 @@
+package J1.S.P0059;
+public class Main {
+
+    public static void main(String[] args) {
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.findPersonInfo();
+                    break;
+                case 2:
+                    Manager.coppyNewFile();
+                    break;
+                case 3:
+                    return;
+            }
+        }
+    }
+}
